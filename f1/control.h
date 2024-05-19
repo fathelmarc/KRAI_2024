fb1be5921b22d57376e74076588646925fba7f57
fathel <125138378+fathelmarc@users.noreply.github.com>
2024-05-19 15:51:44 +0700

    Update control.h

--- f1/control.h
+++ f1/control.h
@@ -1,6 +1,12 @@
 #ifndef CONTROL_H
 #define CONTROL_H
 #include "fethernet.h"
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <termios.h>
+#include <unistd.h>
+#include <string>
 
 #define LeftHatX 0
 #define LeftHatY 1
@@ -8,30 +14,62 @@
 #define RightHatY 4
 #define SILANG 0
 #define CIRCLE 1
-const int CIRCLE1 = -1;
 #define TRIANGLE 2
 #define SQUARE 3
-const int SQUARE1 = -3;
 #define L1 4
 #define R1 5
 #define L2 2
 #define R2 5
-#define butY 7 
-#define butX 6
-#define UP 11
-#define DOWN -11
+
+const int CIRCLE1 = -1;
+const int SQUARE1 = -3;
+
 #define MINSPEED 0.8
 #define MAXSPEED 2
 #define MAXSPEEDT 0.4
 #define hold 169
+#define startGripper 90
 
+#define ax_y 7 //axis < 0
+// #define down 7 //axis >0
+#define ax_x  6 //axis >0
+// #define right 6 //axis <0
+#define triangle 2//button
+#define circle 1//button
+#define square 3//button
+// #define ex 0 //button
+#define ps 10 //button
+#define option 9//button
+#define share 8 //button
 
 int caseBot,manual;
 int prevcase;
 int moveImu;
 static int speed;
-float speedChange[4] = {1.3,2,3,7};
+float speedChange[4] = {1.3,2,3,4};
 float speedChangeT[4] = {1.5,2,3,4};
+
+string getKeyPress() {
+    char key;
+    struct termios oldt, newt;
+
+    // Save current terminal settings
+    tcgetattr(STDIN_FILENO, &oldt);
+
+    // Set terminal to non-canonical mode (no buffering)
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+    // Read a single character
+    read(STDIN_FILENO, &key, 1);
+
+    // Restore terminal settings
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+
+    return string(1, key);
+}
+
 
 struct Joystick
 {
@@ -75,7 +113,37 @@ void readJoystickInput(Joystick* joystick)
         }
     }
 }
+// string key = getKeyPress();
+// void keyboardControl(){
+//     static int s_pressed = 0;
+//     static int x_pressed = 0;
+//     if(key == "s" && !s_pressed){
+//         caseBot += 1;
+//         s_pressed = 1;
+//     }if(key != "s"){
+//         s_pressed = 0;
+//     }
+//     if(key == "x" && !x_pressed){
+//         caseBot += 1;
+//         x_pressed = 1;
+//     }if(key != "x"){
+//         x_pressed = 0;
+//     }
+//     if(key == "A"){
+//         //maju
+//     }if(key == "B"){
+//         //mundur
+//     }if(key == "C"){
+//         //kanan
+//     }if(key == "D"){
+//         //kiri
+//     }
+// }
 
+
+static int prevTanjakkan = 0;
+static int tanjakkan = 0;
+static int afterTanjakkan = 0;
 const char* fileName = "/dev/input/js0";
 Joystick joy = openJoystick(fileName);
 void inCaseAuto(){
@@ -85,14 +153,32 @@ void inCaseAuto(){
     //kecepatan
     static int axisCont = 0;
     static int prevAxisValue = 0;
-    if (joy.axisStat[7] > prevAxisValue && !axisCont) {
+    static int send_gripper_ball = 0;
+    if(pitch <= -5 && afterTanjakkan == 0){
+        tanjakkan = prevTanjakkan + 1;
+        prevTanjakkan = tanjakkan;
+
+        afterTanjakkan = 1;
+    }else if(pitch >= -1 && afterTanjakkan == 1){
+        tanjakkan = prevTanjakkan + 1;
+        prevTanjakkan = tanjakkan;
+        afterTanjakkan = 2;
+    }
+    if(tanjakkan >= 1 && send_gripper_ball == 0){
+        for(int i = 0; i <3000; i++){
+            atas.kirimData(sockfd, std::to_string(39));
+        }
+        send_gripper_ball = 1;
+    }
+    // cout<<tanjakkan<<endl;
+    if (joy.axisStat[ax_y] > prevAxisValue && !axisCont) {
         speed--;
         axisCont =1;
     }
-    if (joy.axisStat[7] < prevAxisValue && !axisCont) {
+    if (joy.axisStat[ax_y] < prevAxisValue && !axisCont) {
         speed++;
         axisCont = 1;
-    }else if(joy.axisStat[7] == 0){
+    }else if(joy.axisStat[ax_y] == 0){
         axisCont = 0;
     }else if(speed >= 4){
         speed = 3;
@@ -100,7 +186,7 @@ void inCaseAuto(){
     }else if(speed <= -1){
         speed = 0;
     }
-    prevAxisValue = joy.axisStat[7];
+    prevAxisValue = joy.axisStat[ax_y];
     // Posisi
     static int silangPressed = 0;
     static int trianglePressed = 0;
@@ -130,33 +216,41 @@ void inCaseAuto(){
         caseBot = 12;
         prevcase = caseBot;
     }
-    if(joy.bStat[10]){
+    if(joy.bStat[ps]){
         manual = 0;
         caseBot = 0;
-    }else if(joy.bStat[9] || joy.bStat[8]){
+    }
+    if(joy.bStat[option]){
         prevcase = 0;
+        afterTanjakkan = 0;
+        prevTanjakkan = 0;
+        tanjakkan = 0;
+        send_gripper_ball = 0;
+        atas.kirimData(sockfd, std::to_string(999));
     }
-
-    if(joy.bStat[7]){
-        moveImu= 1;
-    }else if(joy.bStat[6]){
-        moveImu = 0;
-    }
+    int lamp[4] = {101,102,103,104};
     switch (speed)
     {
     case 0:
-        atas.kirimData(sockfd,std::to_string(101));
+        atas.kirimData(sockfd, std::to_string(lamp[0]));
         break;
     case 1:
-        atas.kirimData(sockfd,std::to_string(102));
+        atas.kirimData(sockfd, std::to_string(lamp[1]));
         break;
     case 2:
-        atas.kirimData(sockfd,std::to_string(103));
+        atas.kirimData(sockfd, std::to_string(lamp[2]));
         break;
     case 3:
-        atas.kirimData(sockfd,std::to_string(104));
+        atas.kirimData(sockfd, std::to_string(lamp[3]));
         break;
     }
     // printf("caseBot = %d\nprevCase =%d\n",caseBot,prevcase);
 }
+// void tanjakkan(){
+//     if(tanjakkan ==1){
+//         adjust(hx, hy, 45, passT);
+//     }else {
+//         inKinematic(hx,hy,ht, passT); 
+//     }
+// }
 #endif
